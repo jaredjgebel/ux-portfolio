12a0579f365fddc9e243f866219098fb73868c5f
Jared <jaredjgebel@gmail.com>
2022-04-27 15:10:04 -0400

    Add configuration options

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,41 @@
-const htmlmin = require('html-minifier')
+const htmlmin = require("html-minifier");
+const markdownIt = require("markdown-it");
+const markdownItClass = require("@toycode/markdown-it-class");
+const mapping = require("./styles/mapStylesToClasses");
 
-const now = String(Date.now())
+const md = markdownIt({ linkify: true });
+md.use(markdownItClass, mapping);
+
+const now = String(Date.now());
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
-  eleventyConfig.addWatchTarget('./styles/tailwind.css')
+  eleventyConfig.addWatchTarget("./styles/tailwind.config.js");
+  eleventyConfig.addWatchTarget("./styles/tailwind.css");
 
   eleventyConfig.addPassthroughCopy({
-    './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
-  })
+    "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
+  });
 
-  eleventyConfig.addShortcode('version', function () {
-    return now
-  })
+  eleventyConfig.addShortcode("version", function () {
+    return now;
+  });
 
-  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (
       process.env.ELEVENTY_PRODUCTION &&
       outputPath &&
-      outputPath.endsWith('.html')
+      outputPath.endsWith(".html")
     ) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
-      })
-      return minified
+      });
+      return minified;
     }
 
-    return content
-  })
-}
+    return content;
+  });
+
+  eleventyConfig.setLibrary("md", md);
+};
---
+++ styles/mapStylesToClasses.js
@@ -0,0 +1,4 @@
+module.exports = {
+  h1: ["text-xl", "md:text-2xl", "font-bold"],
+  a: ["text-blue-400", "hover:underline"],
+};
